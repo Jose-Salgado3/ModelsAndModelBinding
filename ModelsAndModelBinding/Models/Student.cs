bd94c77b6a28cab564d395d5481d03607df73eae
Jose Salgado <Josalgado03@gmail.com>
2019-07-27 00:59:21 -0700

    Added data annotations to student model

--- ModelsAndModelBinding/Models/Student.cs
+++ ModelsAndModelBinding/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Primitives;
@@ -18,19 +19,27 @@ namespace ModelsAndModelBinding.Models
         /// The legal First and Last name
         /// ex. J.Doe
         /// </summary>
+        [Required(ErrorMessage = "Gimme that name!")]
+        [Display(Name = "First and Last name")]
         public string FullName { get; set; }
 
         /// <summary>
         /// The program the student intends to complete
         /// </summary>
+
+        [Required]
+        [Display(Name = "Choose Program")]
         public string ProgramOfChoice { get; set; }
 
-
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
 
         /// <summary>
         /// The mailing address of the student that is registering
         /// </summary>
+        
+        // Max 30 characters for demo
+        [StringLength(30, MinimumLength = 5)]
         public string HomeAddress { get; set; }
     }
 }
